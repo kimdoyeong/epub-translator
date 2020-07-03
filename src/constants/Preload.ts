983d02037dbb10ccf33b0ff257c6585ac47dd5df
Doyeong Kim <48087967+kimdoyeong@users.noreply.github.com>
2020-07-03 23:08:40 +0900

    Update open dialog

---
+++ src/constants/Preload.ts
@@ -0,0 +1,5 @@
+import { PreloadType } from "../preload";
+
+const Preload: PreloadType = (window as any).preloads;
+
+export default Preload;
--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -2,9 +2,14 @@ import React from "react";
 import styled from "styled-components";
 import { MdFileUpload } from "react-icons/md";
 import FullLayout from "../components/FullLayout";
+import Preload from "../constants/Preload";
 
 function IndexPage() {
-  function selectFile() {}
+  function selectFile() {
+    Preload.Dialog.openEpubDialog().then((v) => {
+      console.log(v);
+    });
+  }
   return (
     <FullLayout>
       <DropZone onClick={selectFile}>
