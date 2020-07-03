745a59b659e5e6c9a23915cb1b43bf09387889d6
Doyeong Kim <48087967+kimdoyeong@users.noreply.github.com>
2020-07-03 18:28:12 +0900

    Update BrowserRouter

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+
 import App from "./App";
 
 const root = document.querySelector("#root");
 
-ReactDOM.render(<App />, root);
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  root
+);
