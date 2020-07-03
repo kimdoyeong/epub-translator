a031af0f6eec1207af06d70795c1937c1ec87158
Doyeong Kim <48087967+kimdoyeong@users.noreply.github.com>
2020-07-04 01:36:43 +0900

    Update InfoPage

--- src/pages/InfoPage.tsx
+++ src/pages/InfoPage.tsx
@@ -1,8 +1,42 @@
 import React from "react";
+import styled from "styled-components";
 import FullLayout from "../components/FullLayout";
+import { useSelector } from "react-redux";
+import { RootState } from "../store";
 
 function InfoPage() {
-  return <FullLayout>Info</FullLayout>;
+  const { title, language, cover } = useSelector(
+    (state: RootState) => state.file.data
+  );
+  return (
+    <FullLayout>
+      <Layout>
+        <div className="left">
+          <img src={cover} alt={title} className="cover" />
+        </div>
+        <div className="right">
+          <h1 className="title">{title}</h1>
+        </div>
+      </Layout>
+    </FullLayout>
+  );
 }
+
 InfoPage.path = "/info";
+
+const Layout = styled.div`
+  display: flex;
+  .left {
+    .cover {
+      min-width: 100px;
+      max-width: 150px;
+      height: auto;
+    }
+  }
+  .right {
+    flex: 1;
+    padding: 1em;
+    box-sizing: border-box;
+  }
+`;
 export default InfoPage;
