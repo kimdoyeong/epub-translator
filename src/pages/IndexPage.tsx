4b7c162845c2b1808fab4727680b69b87e2e4157
Doyeong Kim <48087967+kimdoyeong@users.noreply.github.com>
2020-07-03 19:09:02 +0900

    Update IndexPage

--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -1,7 +1,36 @@
 import React from "react";
+import styled from "styled-components";
+import { MdFileUpload } from "react-icons/md";
+import FullLayout from "../components/FullLayout";
 
 function IndexPage() {
-  return <div>MainPage</div>;
+  return (
+    <FullLayout>
+      <DropZone>
+        <MdFileUpload style={{ fontSize: 48 }} />
+        <span style={{ marginTop: 16 }}>
+          클릭해서 선택하거나 파일을 드래그 앤 드랍
+        </span>
+      </DropZone>
+    </FullLayout>
+  );
 }
+
+const DropZone = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 1em;
+  box-sizing: border-box;
+  border: 2px dotted gray;
+  transition: background 1s;
+  cursor: pointer;
+
+  :hover {
+    background: #eaeaea;
+  }
+`;
 
 export default IndexPage;
