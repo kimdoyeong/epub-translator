67aa3aa76abf46218edda46b9f961b929a163e0e
Doyeong Kim <48087967+kimdoyeong@users.noreply.github.com>
2020-07-06 18:58:00 +0900

    Remove unnecessary import

--- src/preload/tempDir.ts
+++ src/preload/tempDir.ts
@@ -1,7 +1,6 @@
 import path from "path";
 import fs from "fs";
 import { remote } from "electron";
-import rimraf from "rimraf";
 
 const tempDir = path.join(remote.app.getPath("temp"), "epub_translator");
 
