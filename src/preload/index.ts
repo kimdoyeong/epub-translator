471383f04b0160af25bdc01653376528594891b2
Doyeong Kim <48087967+kimdoyeong@users.noreply.github.com>
2020-07-06 19:00:14 +0900

    Update filename

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -2,7 +2,7 @@ import * as Dialog from "./dialog";
 import * as Parsing from "./parsing";
 import PreferenceManager from "./PreferenceManager";
 import translate from "./translate";
-import Store from "./store";
+import Store from "./Store";
 
 const preloads = {
   Dialog,
